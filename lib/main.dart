688dfb5ca69f5781bdc7c8d633ea1d8960586187
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2025-03-07 11:08:30 -0300

    [FEATURE] - Register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:allou/pages/login.dart';
+import 'package:allou/pages/register.dart';
 import 'package:allou/utils/themes.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: lightMode,
-      home: const Scaffold(body: LoginPage()),
+      home: const Scaffold(body: RegisterPage()),
     );
   }
 }
