d64ffda6a9edf9cc6b43c648a26e2b2c526e8a18
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2025-03-11 11:12:58 -0300

    [FEATURE] - Setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:allou/firebase_options.dart';
 import 'package:allou/pages/auth.dart';
 import 'package:allou/utils/themes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
